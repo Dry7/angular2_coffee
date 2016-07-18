bb4ad2d65df5cbdbdfd3b7e29af45290e2d513d9
Dry7 <andrey.dry7.shilov@yandex.ru>
2016-07-18 15:40:26 +0300

    Dashboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {ROUTER_DIRECTIVES} from "@angular/router";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {MenuTopComponent} from "./components/menu-top/menu-top.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, MenuTopComponent]
 })
 
 export class AppComponent {
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductsComponent} from "../products/products.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-dashboard',
+  directives: [ProductsComponent],
   templateUrl: 'dashboard.component.html',
   styleUrls: ['dashboard.component.css']
 })
